64ee900dc604575943ac0f8b53a07cfdde48b6cb
Felipe McChicken <aa@mail.com>
2019-03-29 11:28:00 +0100

    the cards are flippig

--- src/app/tp/tp.page.ts
+++ src/app/tp/tp.page.ts
@@ -14,17 +14,51 @@ export class TpPage implements OnInit {
 
   private questionMarkUrl = '/assets/images/question-mark.png';
 
+  /**
+   * Génération du jeu de cartes
+   **/
+
   private generateDeck() {
     for (let i = 0; i < this.numberOfDistinctCards; i++) {
       let imgUrl = '/assets/images/cards/' + i + ".png";
       this.cardList.push({ img: this.questionMarkUrl, name: imgUrl, revealed: false });
       this.cardList.push({ img: this.questionMarkUrl, name: imgUrl, revealed: false });
     }
+    this.shuffleDeck();
   }
+
+  /** 
+   * Mélange des cartes
+   */
+  private shuffleDeck(){
+    this.cardList.forEach(
+      (item, index, deck) => {
+        //position aléatoire dans le tableau
+      let newPos = Math.floor(Math.random() * deck.length);
+        //permutation 
+        deck[index] = deck[newPos];
+        deck[newPos] = item;
+    });
+  }
+
+  private flipCard(card, index){
+    //affichage de la carte
+    card.img = card.name;
+    card.revealed=true;
+    //masquage de la carte au bout de 1 seconde
+    setTimeout(
+      ()=>{
+        card.img = this.questionMarkUrl;
+        card.revealed = false;
+      },
+      1000);
+  }
+
 
 
   constructor() { 
     this.generateDeck();
+    console.log(this.cardList);
   }
 
   ngOnInit() {
